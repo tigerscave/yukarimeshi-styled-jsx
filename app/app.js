57e6cc1388e1b3bd246961e74288992140174c8f
tadaniya <t.adaniya@adawarp.com>
2019-11-10 17:42:09 +0900

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+	return (
+		<div>
+			<p>hoge</p>
+			<style jsx>{`
+				p {
+					color: red;
+				}
+				`}</style>
+		</div>)
+}
+
+export default App
